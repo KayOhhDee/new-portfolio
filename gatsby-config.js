7bf608b555b02fddc2e4625aff4b062dd5075abb
kod3180 <kod3180@gmail.com>
2022-01-02 18:37:40 +0000

    modify site meta data, configure styled components, fonts plugin and contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Kwasi Owusu-Donkor`,
+    description: `I am a Frontend Software Engineer who endeavours to construct dynamic and beautiful web applications through code that is carefully crafted and user-centeric design.`,
+    author: `@kwasidonkor`,
+    keywords: `web developer, frontend developer, software engineer, ghana frontend developer, react developer, vue developer, javascript developer`,
+    siteUrl: `https://kwasidonkor.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,22 +22,47 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     // This will impact how browsers show your PWA/website
-    //     // https://css-tricks.com/meta-theme-color-and-trickery/
-    //     // theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Poppins",
+              variants: ["400", "500", "600", "700"],
+            },
+            {
+              family: "Roboto Mono",
+              variants: ["400", "600"],
+            },
+          ],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `k53nnh8ddz9s`,
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kwasi O.Donkor`,
+        short_name: `Kwasi O.Donkor`,
+        start_url: `/`,
+        background_color: `#23263a`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
